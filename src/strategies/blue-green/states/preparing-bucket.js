f28a1002a9cfef836384fbc362ad5ffe5d044f0c
Brendan McMahon <bernos@gmail.com>
2015-05-26 22:02:10 +1000

    Started tidying up steps

--- src/strategies/blue-green/states/preparing-bucket.js
+++ src/strategies/blue-green/states/preparing-bucket.js
@@ -4,63 +4,60 @@ var Q = require('q'),
     helpers = require('../../../lib/helpers');
 
 module.exports = function(config, args) {
+    var s3 = new config.services.AWS.S3();
 
-    var s3      = new config.services.AWS.S3();
-
-    function createBucketIfNotExists(bucket, region) {
-        return listBuckets().then(function(result) {
-            if (_.any(result.Buckets, { Name : bucket })) {
-                l.info("Bucket %s already exists.", bucket);
-                return waitForBucket(bucket);
-            } 
-
-            return createBucket(bucket, region);            
-        });
-    }
-
-    function listBuckets() {
-        return Q.ninvoke(s3, "listBuckets", {});
-    }
-
-    function createBucket(bucket, region) {
-        
-        l.info("Creating bucket %s in region %s.", bucket, region);
-
-        return Q.ninvoke(s3, "createBucket", {
-            Bucket : bucket,
-            ACL    : 'private',
-            CreateBucketConfiguration : {
-                LocationConstraint : region
-            }
-        })
-        .then(function(result) {
-            l.success("Created bucket %s in region %s.", bucket, region);
-            return waitForBucket(bucket);
-        });
-    }
-    
-    function waitForBucket(bucket) {
-        
-        l.info("Waiting for bucket %s to be ready.", bucket);
-
-        return Q.ninvoke(s3, "waitFor", "bucketExists", {
-            Bucket : bucket
-        })
-        .then(function(result) {
-            l.info("Bucket %s is ready.", bucket);
-            return result;
-        });     
-    }
-
-    return {
+	return {
         activate : function(fsm, data) {
             data.bucket = calculateBucketName(config);
             
-			createBucketIfNotExists(data.bucket, config.Region)
+			createBucketIfNotExists(s3, data.bucket, config.Region)
                 .then(helpers.genericContinue(fsm, data))
                 .fail(helpers.genericRollback(fsm, data));
         }
     }
+}
+
+function createBucketIfNotExists(s3, bucket, region) {
+    return listBuckets(s3)
+		.then(function(result) {
+            if (_.any(result.Buckets, { Name : bucket })) {
+                l.info("Bucket %s already exists.", bucket);
+                return waitForBucket(s3, bucket);
+            } 
+            return createBucket(s3, bucket, region);            
+        });
+}
+
+function listBuckets(s3) {
+    return Q.ninvoke(s3, "listBuckets", {});
+}
+
+function createBucket(s3, bucket, region) {
+    l.info("Creating bucket %s in region %s.", bucket, region);
+
+	return Q.ninvoke(s3, "createBucket", {
+        Bucket : bucket,
+        ACL    : 'private',
+        CreateBucketConfiguration : {
+            LocationConstraint : region
+        }
+    })
+    .then(function(result) {
+        l.success("Created bucket %s in region %s.", bucket, region);
+        return waitForBucket(s3, bucket);
+    });
+}
+    
+function waitForBucket(s3, bucket) {
+    l.info("Waiting for bucket %s to be ready.", bucket);
+
+    return Q.ninvoke(s3, "waitFor", "bucketExists", {
+        Bucket : bucket
+    })
+    .then(function(result) {
+        l.info("Bucket %s is ready.", bucket);
+        return result;
+    });     
 }
 
 function calculateBucketName(config) {
